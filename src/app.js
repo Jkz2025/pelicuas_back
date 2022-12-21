4c0ffb673f6460654e794a1f13f2204947be8e17
Joel Vasquez <joeldavidvasquezrivas@gmail.com>
2022-12-21 16:23:02 -0500

    Habilitacion de cores

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import express from 'express'
 import {BD} from './db.js'
 import {PORT} from './config.js'
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 const app = express()
 
 app.get('/', async (req, res) => {
